137bbfdadd8ae8219046e606b2478a1d36e3bab5
Juan Ramirez <Juan.Avendano@Solera.com>
2022-09-22 12:10:16 -0500

    First commit

---
+++ Vidly/Controllers/Api/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomerController : ApiController
+    {
+        private ApplicationDbContext _context;  
+
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // Get /api/customer
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+    }
+}
